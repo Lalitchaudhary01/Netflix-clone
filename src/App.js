031c6b86665864cd1d1e2ae60e5b4d8e7eb86723
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-03-08 20:55:50 +0530

    add react-router-provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
+import React from "react";
+import Body from "./components/Body";
 
 function App() {
-  return <div className="text-3xl font-bold text-red-800">Netflix</div>;
+  return <Body />;
 }
 
 export default App;
