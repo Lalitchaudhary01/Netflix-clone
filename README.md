963dcd3deb8b4a9f84a01b6b124ee36483277ce2
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-03-11 00:21:57 +0530

    use Ref hooks

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # netflix GPT
 -Create React app
 -configured tailwind css
+-Header
+-Routing of app
+-Login Form
+-Sign up Form
+-Form validation
+-useRef Hook
+
 
 # features
 -login/sign up
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    //Validate the form data
+    const message = checkValidData(email.current.value, password.current.value);
+    console.log(message);
+  };
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -16,7 +26,10 @@ const Login = () => {
           alt="logo"
         />
       </div>
-      <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
         <h1 className="font-bold text-3xl py-4 ">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -28,16 +41,22 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-800"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-800"
         />
-        <button className="p-4 my-6 w-full bg-red-700  rounded-lg">
+        <p></p>
+        <button
+          className="p-4 my-6 w-full bg-red-700  rounded-lg"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
