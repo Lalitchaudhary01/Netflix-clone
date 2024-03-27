8c36b2af394af7036e82129800e32776a9db351d
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-03-27 23:42:03 +0530

    add autthentication for sign in aur sing out  by get auth

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./Login";
 import Browse from "./Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { auth, onAuthStateChanged } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -14,6 +19,22 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ udi: uid, email, displayName: displayName }));
+        // ...
+      } else {
+        dispatch(removeUser());
+        // User is signed out
+        // ...
+      }
+    });
+  }, []);
 
   return (
     <div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,21 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-20">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
+      <div className="flex items-center">
+        <img
+          className="w-12 h-12 rounded-full"
+          alt="usericon"
+          src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
+        />
+        <button className="font-bold ml-2">(Sign Out)</button>{" "}
+        {/* Added margin to the button */}
+      </div>
     </div>
   );
 };
